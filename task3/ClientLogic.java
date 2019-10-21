4624588f208218ce57077c336de06c3e5f1ee333
juliekazlova <55464729+juliekazlova@users.noreply.github.com>
2019-10-21 23:10:25 +0300

    Update ClientLogic.java

--- task3/ClientLogic.java
+++ task3/ClientLogic.java
@@ -1,48 +1,49 @@
 package by.epam.java.kazlova.task3;
 
+
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class ClientLogic {
     public float currentSum(Client client){
         float sum=0;
-        for(int i=0; i<client.getAccounts().length; i++){
-            if(!client.getAccounts()[i].isBlocked()) {
-                sum += client.getAccounts()[i].getSum();
-            }
+        for(Account cur: client.getAccounts()){
+            sum+=cur.getSum();
         }
         return sum;
     }
 
     public float currentSumNegative(Client client){
         float sum=0;
-        for(int i=0; i<client.getAccounts().length; i++){
-           if(!client.getAccounts()[i].isBlocked()&&client.getAccounts()[i].getSum()<0)
-            sum+=client.getAccounts()[i].getSum();
+        for(Account cur: client.getAccounts()){
+           if(!cur.isBlocked()&&cur.getSum()<0)
+            sum+=cur.getSum();
         }
         return sum;
     }
 
     public float currentSumPositive(Client client){
         float sum=0;
-        for(int i=0; i<client.getAccounts().length; i++){
-            if(!client.getAccounts()[i].isBlocked()&&client.getAccounts()[i].getSum()>0)
-                sum+=client.getAccounts()[i].getSum();
+        for(Account cur: client.getAccounts()){
+            if(!cur.isBlocked()&&cur.getSum()>0)
+                sum+=cur.getSum();
         }
         return sum;
     }
 
     public Account findAccount(Client client, int id){
-        for(int i=0; i<client.getAccounts().length; i++){
-            if(client.getAccounts()[i].getId()==id){
-                return client.getAccounts()[i];
+        for(Account cur: client.getAccounts()){
+            if(cur.getId()==id){
+                return cur;
             }
         }
         return null;
     }
 
-    public Account[] sortAccounts(Client client){
-        Account[] sorted=new Account[client.getAccounts().length];//?
-       return null;
+    public ArrayList<Account> sortAccounts(Client client){
+        ArrayList<Account> sorted= new ArrayList<>(client.getAccounts());
+                Collections.sort(sorted);
+       return sorted;
     }
 }
