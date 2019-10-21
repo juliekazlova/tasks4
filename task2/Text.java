3eedae46b404f0630ae57036a59591608d49deb6
juliekazlova <55464729+juliekazlova@users.noreply.github.com>
2019-10-21 22:51:52 +0300

    Update Text.java

--- task2/Text.java
+++ task2/Text.java
@@ -3,6 +3,9 @@ package by.epam.java.kazlova.task2;
 //Создать объект класса Текст, используя классы Предложение, Слово. Методы: дополнить текст, вывести на
 //консоль текст, заголовок текста.
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Text {
     private Word name;
     private Sentence[] text;
@@ -56,5 +59,21 @@ public class Text {
             sb.append(text[i].getClause()).append(" ");
         }
         return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Text)) return false;
+        Text text1 = (Text) o;
+        return Objects.equals(getName(), text1.getName()) &&
+                Arrays.equals(getText(), text1.getText());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(getName());
+        result = 31 * result + Arrays.hashCode(getText());
+        return result;
     }
 }
