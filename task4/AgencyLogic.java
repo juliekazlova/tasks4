97ee9a7207b7e57e85d725ac46e13e8d130d8975
juliekazlova <55464729+juliekazlova@users.noreply.github.com>
2019-10-21 23:50:00 +0300

    Update AgencyLogic.java

--- task4/AgencyLogic.java
+++ task4/AgencyLogic.java
@@ -1,4 +1,39 @@
 package by.epam.java.kazlova.task4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AgencyLogic {
+
+    public List<Trip> choseByTransport(Agency agency, TransportType transportType){
+        List<Trip> trips=new ArrayList<>();
+        for(Trip cur: agency.getTrips()){
+            if(cur.getTransport()==transportType){
+                trips.add(cur);
+            }
+        }
+        return trips;
+    }
+
+    public List<Trip> choseByMeal(Agency agency, MealType mealType){
+        List<Trip> trips=new ArrayList<>();
+        for(Trip cur: agency.getTrips()){
+            if(cur.getMeal()==mealType){
+                trips.add(cur);
+            }
+        }
+        return trips;
+    }
+
+    public List<Trip> choseByDaysNumber(Agency agency, int days) {
+        List<Trip> trips=new ArrayList<>();
+        for(Trip cur: agency.getTrips()){
+            if(cur.getDaysCount()<=days){
+                trips.add(cur);
+            }
+        }
+    return trips;
+    }
+
+
 }
